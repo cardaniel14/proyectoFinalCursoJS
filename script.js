a9cb0b60c0966699f9c2900867be6e985e53dc29
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-07-13 00:46:32 +0100

    creando repo de proyecto final

---
+++ script.js
@@ -0,0 +1,11 @@
+let btnHistoria = document.querySelector(".boton");
+let btnJuego = document.querySelector(".btnjuego");
+
+btnHistoria.addEventListener("click", ()=>{
+    window.location.href= "historia.html";
+    
+});
+
+btnJuego.addEventListener("click", ()=>{
+    window.location.href = "juego.html";
+});
